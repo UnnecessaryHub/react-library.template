cc19a1fa09366aed83f9b2c87d33e39a22626674
Gearonix <egor.uzhanin@gmail.com>
2023-10-18 09:38:24 +0300

    Removed .babelrc

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import { defineConfig }  from 'vite'
 export default defineConfig({
   cacheDir: './node_modules/.vite/project',
 
-  plugins: [nxViteTsPaths()],
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest'
-    },
-    environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
-  }
+  plugins: [nxViteTsPaths()]
 })
